95412522b95dcbbfc0d428c43332ae1b3f478e71
misha <misha@DESKTOP-V5I69QC>
2022-04-12 14:17:43 +0200

    Добавьте файлы проекта.

---
+++ Class training/MaxNumFunc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_training
+{
+    public class MaxNumFunc
+    {
+
+
+        public int MaxR;
+
+
+
+        public void MaxNum(int a, int b) 
+        { 
+        if (a>=b)
+            {
+                MaxR = a;
+                
+            }
+        if (b > a)
+            {
+                MaxR = b;
+                
+            }
+        else
+            {
+                Console.WriteLine("Error");
+                
+
+            }
+
+
+        }
+    }
+        
+
+
+}
+
---
+++ Class training/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Class_training
+{
+    class Program
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            int a;
+            List<int?> listok = new List<int?>();
+            string input1;
+            Console.WriteLine("Which number is higher, input numbers");
+            int counter = 0;
+            int b;
+            do
+            {
+                input1 = Console.ReadLine();
+                if ((string.IsNullOrEmpty(input1)) == true)
+                {
+                    counter++;
+                }
+                else
+                {
+                    a = Convert.ToInt32(input1);
+                    listok.Add(a);
+                }
+                
+            }
+            while (counter < 1);
+
+
+            //Console.WriteLine(listok.ToString());
+            Console.WriteLine(string.Join(", ", listok));
+
+            var mc = new MaxNumFunc();
+            a = (int)listok[0];
+
+            for (int i = 0; i < listok.Count(); i++ )
+            {
+                b = (int)listok[i];
+
+                mc.MaxNum(a, b);
+
+                a = mc.MaxR;
+            }
+
+            Console.WriteLine(a);
+
+
+
+
+
+        }
+    }
+}
